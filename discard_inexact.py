307a3c56671c82e47e4a0ef2dd2e937b35cc8e53
Leo Scherer <lenoxy@gmx.ch>
2023-10-06 17:38:17 +0200

    add batch processing

--- discard_inexact.py
+++ discard_inexact.py
@@ -1,4 +1,5 @@
 import datetime
+from os import walk, listdir
 from typing import Type
 
 import gpxpy
@@ -24,42 +25,52 @@ def is_exact(point_a, point_b) -> bool:
     return geopy.distance.geodesic(point_a, point_b).m < 300
 
 
-gpx_input_file = open('./out/2023-6.gpx', 'r')
-old_gpx = gpxpy.parse(gpx_input_file)
-new_gpx = gpxpy.gpx.GPX()
 
-for track in old_gpx.tracks:
-    mode = track.type
-    single_segment = track_segments_to_single_segment(track)
+for file_name in listdir('./out/'):
+    print(file_name)
+    if not file_name.endswith(".gpx"):
+        print("skipping since file {0} does not end with .gpx".format(file_name))
+        continue
+
+    gpx_input_file = open('./out/{0}'.format(file_name), 'r')
+    old_gpx = gpxpy.parse(gpx_input_file)
+    new_gpx = gpxpy.gpx.GPX()
+
+    for track in old_gpx.tracks:
+        mode = track.type
+        single_segment = track_segments_to_single_segment(track)
 
 
-    def save_segment(segment):
-        track.segments.append(segment)
+        def save_segment(segment):
+            track.segments.append(segment)
 
 
-    def create_loop_segment() -> GPXTrackSegment:
-        return GPXTrackSegment()
+        def create_loop_segment() -> GPXTrackSegment:
+            return GPXTrackSegment()
 
 
-    loop_segment = create_loop_segment()
-    has_point_to_save = False
-    for point in single_segment.points:
-        current_coordinates = (point.longitude, point.latitude)
-        if has_point_to_save and not is_exact(current_coordinates, last_coordinates):
-            save_segment(loop_segment)
-            loop_segment = create_loop_segment()
-            has_point_to_save = False
-        else:
-            has_point_to_save = True
-            loop_segment.points.append(point)
-        last_coordinates = (point.longitude, point.latitude)
+        loop_segment = create_loop_segment()
+        has_point_to_save = False
+        for point in single_segment.points:
+            try:
+                current_coordinates = (point.longitude, point.latitude)
+                if has_point_to_save and not is_exact(current_coordinates, last_coordinates):
+                    save_segment(loop_segment)
+                    loop_segment = create_loop_segment()
+                    has_point_to_save = False
+                else:
+                    has_point_to_save = True
+                    loop_segment.points.append(point)
+                last_coordinates = (point.longitude, point.latitude)
+            except:
+                print("warning")
+                continue
+        print("appending track {0}...".format(mode))
+        track.type = mode
+        new_gpx.tracks.append(track)
 
-    print("appending track {0}...".format(mode))
-    track.type = mode
-    new_gpx.tracks.append(track)
-
-new_gpx.waypoints = old_gpx.waypoints
-xml = new_gpx.to_xml()
-gpx_output_file = open('output_file.gpx', 'w')
-gpx_output_file.write(xml)
-gpx_output_file.close()
+    new_gpx.waypoints = old_gpx.waypoints
+    xml = new_gpx.to_xml()
+    gpx_output_file = open('./out/exact/{0}'.format(file_name), 'w')
+    gpx_output_file.write(xml)
+    gpx_output_file.close()
